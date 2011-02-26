1a1deb92964885a1a44fbb09a75388a5848783d4
Matt Ingenthron <ingenthr@cep.net>
2011-02-25 17:20:00 -0800

    Remove standard Authenticator and replace with simple auth header.
    It was found that the Authenticator was caching within the process
    in some cases and in others just plain wasn't working.  Since
    nothing more than HTTP Basic is required at the moment, just add
    our own header.
    
    Change-Id: I6bfce8ba7b51b76c8b99149405b0caadbe60815d

--- src/test/java/net/spy/memcached/vbucket/ConfigurationProviderHTTPTest.java
+++ src/test/java/net/spy/memcached/vbucket/ConfigurationProviderHTTPTest.java
@@ -46,4 +46,9 @@ public class ConfigurationProviderHTTPTest extends TestCase {
     public void testGetAnonymousAuthBucket() throws Exception {
         assertEquals("default", configProvider.getAnonymousAuthBucket());
     }
+
+    public void testBuildAuthHeader() {
+	ConfigurationProviderHTTP.buildAuthHeader("foo", "bar");
+
+    }
 }
